81c46794b751dab8c334b09f73041e047c053a5e
Srikanthyadav8142 <srikanthuduthala567@gmail.com>
2026-01-05 10:32:03 +0530

    login print changed

--- github/src/main/java/com/example/github/controller/HelloController.java
+++ github/src/main/java/com/example/github/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        System.out.println("surendar");
+        System.out.println("vamshi");
         return "hello srikanth yadav";
     }
 }
